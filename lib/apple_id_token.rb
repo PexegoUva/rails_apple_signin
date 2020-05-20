df5ee9ffb3ab41a236bf20422c240eb93b3855ef
PexegoUva <samuelvv22@gmail.com>
2020-05-20 20:42:21 +0200

    Add gem to test requests. Add main error cases in spec

--- lib/apple_id_token.rb
+++ lib/apple_id_token.rb
@@ -12,26 +12,28 @@ module AppleIdToken
     APPLE_ISSUER = 'https://appleid.apple.com'
     APPLE_JWKS_URI = 'https://appleid.apple.com/auth/keys'
 
-    def validate(token:, aud:)
-      if get_public_keys
-        payload = check_against_certs(token, aud)
+    class << self
+      def validate(token:, aud:)
+        if get_public_keys
+          payload = check_against_certs(token, aud)
 
-        unless payload
-          raise SignatureError, 'Token not verified as issued by Apple'
+          unless payload
+            raise SignatureError, 'Token not verified as issued by Apple'
+          end
+        else
+          raise PublicKeysError, 'Unable to retrieve Apple public keys'
         end
-      else
-        raise PublicKeysError, 'Unable to retrieve Apple public keys'
       end
-    end
 
-    private
+      private
 
-    def get_public_keys
-      # TODO -> Implement
-    end
+      def get_public_keys
+        # TODO -> Implement
+      end
 
-    def check_against_certs(token, aud)
-      # TODO -> Implement
+      def check_against_certs(token, aud)
+        # TODO -> Implement
+      end
     end
   end
 end
--- spec/apple_id_token_spec.rb
+++ spec/apple_id_token_spec.rb
@@ -1,9 +1,77 @@
-RSpec.describe AppleIdToken do
-  it "has a version number" do
-    expect(AppleIdToken::VERSION).not_to be nil
-  end
+require 'spec_helper'
+require 'fakeweb'
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe AppleIdToken::Validator do
+  describe '.validate' do
+    subject(:validator) { AppleIdToken::Validator }
+
+    let(:fake_token) { 'definitely_a_fake_token' }
+    let(:fake_aud) { 'not_a_real_user_id' }
+
+    context 'when unable to retrieve Apple public keys' do
+      before do
+        FakeWeb::register_uri(:get,
+          AppleIdToken::Validator::APPLE_JWKS_URI,
+          status: ['404', 'Not found'],
+          body: 'It seems that our little goblins were not able to give you the info you wanted. Try another one.'
+        )
+      end
+
+      it 'raises PublicKeysError' do
+        expect {
+          validator.validate(token: fake_token, aud: fake_aud)
+        }.to raise_error(AppleIdToken::PublicKeysError)
+      end
+    end
+
+    context 'when valid public keys' do
+      before do
+        FakeWeb::register_uri(:get,
+          AppleIdToken::Validator::APPLE_JWKS_URI,
+          status: ['200', 'OK'],
+          body: {
+            "keys": [
+              {
+                "kty": "RSA",
+                "kid": "86D88Kf",
+                "use": "sig",
+                "alg": "RS256",
+                "n": "iGaLqP6y-SJCCBq5Hv6pGDbG_SQ11MNjH7rWHcCFYz4hGwHC4lcSurTlV8u3avoVNM8jXevG1Iu1SY11qInqUvjJur--hghr1b56OPJu6H1iKulSxGjEIyDP6c5BdE1uwprYyr4IO9th8fOwCPygjLFrh44XEGbDIFeImwvBAGOhmMB2AD1n1KviyNsH0bEB7phQtiLk-ILjv1bORSRl8AK677-1T8isGfHKXGZ_ZGtStDe7Lu0Ihp8zoUt59kx2o9uWpROkzF56ypresiIl4WprClRCjz8x6cPZXU2qNWhu71TQvUFwvIvbkE1oYaJMb0jcOTmBRZA2QuYw-zHLwQ",
+                "e": "AQAB"
+              },
+              {
+                "kty": "RSA",
+                "kid": "eXaunmL",
+                "use": "sig",
+                "alg": "RS256",
+                "n": "4dGQ7bQK8LgILOdLsYzfZjkEAoQeVC_aqyc8GC6RX7dq_KvRAQAWPvkam8VQv4GK5T4ogklEKEvj5ISBamdDNq1n52TpxQwI2EqxSk7I9fKPKhRt4F8-2yETlYvye-2s6NeWJim0KBtOVrk0gWvEDgd6WOqJl_yt5WBISvILNyVg1qAAM8JeX6dRPosahRVDjA52G2X-Tip84wqwyRpUlq2ybzcLh3zyhCitBOebiRWDQfG26EH9lTlJhll-p_Dg8vAXxJLIJ4SNLcqgFeZe4OfHLgdzMvxXZJnPp_VgmkcpUdRotazKZumj6dBPcXI_XID4Z4Z3OM1KrZPJNdUhxw",
+                "e": "AQAB"
+              }
+            ]
+          }
+        )
+      end
+
+      it 'validates token' do
+        # TODO -> Implement
+      end
+
+      it 'returns payload with provided info' do
+        # TODO -> Implement
+      end
+
+      context 'and no payload found' do
+        before do
+          allow(validator).to receive(:get_public_keys).and_return(true)
+          allow(validator).to receive(:check_against_certs).with(fake_token, fake_aud).and_return(nil)
+        end
+
+        it 'raises SignatureError' do
+          expect {
+            validator.validate(token: fake_token, aud: fake_aud)
+          }.to raise_error(AppleIdToken::SignatureError)
+        end
+      end
+    end
   end
 end
