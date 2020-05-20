a75d2091e95c0b7a37530a14afe61a8f1cba8a12
PexegoUva <samuel@vitcord.com>
2020-05-20 19:09:11 +0200

    Init gem files. Add runtime dependencies.

---
+++ lib/apple_id_token.rb
@@ -0,0 +1,6 @@
+require "apple_id_token/version"
+
+module AppleIdToken
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/apple_id_token_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe AppleIdToken do
+  it "has a version number" do
+    expect(AppleIdToken::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
