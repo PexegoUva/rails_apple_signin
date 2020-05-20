a15c23e2c20114f66b66e7d8a3370b5cc1379a7c
PexegoUva <samuel@vitcord.com>
2020-05-20 19:50:33 +0200

    Add gem skeleton

--- lib/apple_id_token.rb
+++ lib/apple_id_token.rb
@@ -1,6 +1,37 @@
-require "apple_id_token/version"
+require 'apple_id_token/version'
+require 'jwt'
+require 'httparty'
+require 'json'
 
 module AppleIdToken
-  class Error < StandardError; end
-  # Your code goes here...
+  class PublicKeysError < StandardError; end
+  class ValidationError < StandardError; end
+  class SignatureError < ValidationError; end
+
+  class Validator
+    APPLE_ISSUER = 'https://appleid.apple.com'
+    APPLE_JWKS_URI = 'https://appleid.apple.com/auth/keys'
+
+    def validate(token:, aud:)
+      if get_public_keys
+        payload = check_against_certs(token, aud)
+
+        unless payload
+          raise SignatureError, 'Token not verified as issued by Apple'
+        end
+      else
+        raise PublicKeysError, 'Unable to retrieve Apple public keys'
+      end
+    end
+
+    private
+
+    def get_public_keys
+      # TODO -> Implement
+    end
+
+    def check_against_certs(token, aud)
+      # TODO -> Implement
+    end
+  end
 end
